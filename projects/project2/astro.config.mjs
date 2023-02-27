cdc45f87d87fe71016beec7d1d3ac9b832c0b4a8
Lpr17717 <112592924+Lpr17717@users.noreply.github.com>
2023-02-27 15:23:07 -0500

    commit

---
+++ projects/project2/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  base: '/assignments/assignment-5',
+  build: {
+    site: 'http://larissarogozin.com/assignments/assignment-5/',
+  }
+});
